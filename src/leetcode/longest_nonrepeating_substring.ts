24832467f2dc3382f79666d1e836b58a6affceb0
Jane Huang <jane.x.huang@gmail.com>
2023-10-25 13:17:22 -0400

    fast longest nonrepeating substring

--- src/leetcode/longest_nonrepeating_substring.ts
+++ src/leetcode/longest_nonrepeating_substring.ts
@@ -34,7 +34,6 @@ interface Datum {
 function suboptimalButWorks(s: string): number {
   let substr = ''
   let maxSubstr = ''
-  let dupeLetter = ''
 
   for (let i = 0; i < s.length; i++) {
     console.log()
@@ -73,72 +72,62 @@ function suboptimalButWorks(s: string): number {
  return maxSubstr.length
 }
 
+
+/*
+
+Runtime
+Details
+88ms
+Beats 41.27%of users with TypeScript
+
+Memory
+Details
+49.02MB
+Beats 40.29%of users with TypeScript
+
+*/
 function lengthOfLongestSubstring(s: string): number {
-  let substr = ''
-  let maxSubstr = ''
+  if (s === '') return 0
+  let cur_i = 0
+  let cur_j = 0
   let tally: any = {}
 
-  for (let i = 0; i < s.length; i++) {
-    console.log()
-    if (i === 0) {
-      substr = s[0]
-      maxSubstr = s[0]
-      tally[s[0]] = 0
-    } else {
-      const lastLetter = s[i - 1]
-      const curLetter = s[i]
-      console.log('substr: ', substr)
-      console.log('lastLetter: ', lastLetter)
-      console.log('curLetter: ', curLetter)
-      console.log('tally[curLetter]: ', tally[curLetter])
-      console.log('substr does not include ', !tally[curLetter])
-      if (lastLetter !== curLetter && tally[curLetter] === undefined) {
-        // add to string
-        console.log('adding to string')
-        substr += curLetter
-        if (substr.length > maxSubstr.length) {
-          maxSubstr = substr
-        }
-        console.log(substr)
-      } else {
-        console.log('do not add to string')
-        if (lastLetter !== curLetter && tally[curLetter] !== undefined) {
-          // readjust string
-          console.log('readjusting to string')
+  let max_i = 0
+  let max_j = 0
+  tally[s[0]] = 0
 
-          let index = substr.match(curLetter).index
-          console.log(index)
-          for (let z = 0; z <= index; z++) {
-            tally[substr[z]] = undefined
+  for (let i = 1; i < s.length; i++) {
+      const curLetter = s[i]
+
+      if (tally[curLetter] === undefined) {
+        // add to string
+        cur_j += 1
+      } else {
+          let index = tally[curLetter]
+          for (let z = cur_i; z <= index; z++) {
+            tally[s[z]] = undefined
           }
 
-          substr = substr.slice(index + 1, substr.length) + curLetter
-
-          console.log('after readjusting')
-          console.log(substr)
-        } 
-        if (lastLetter === curLetter) {
-          console.log('restarting a substring')
-          substr = s[i]
-          Object.keys(tally).forEach(t => tally[t] = undefined)
-        }
+          cur_i = index + 1
+          tally[curLetter] = i
+          cur_j += 1
       }
       tally[curLetter] = i
+
+    if (cur_j - cur_i   > max_j - max_i) {
+      max_j = cur_j
+      max_i = cur_i
     }
   }
-  if (substr.length > maxSubstr.length) {
-    maxSubstr = substr
-  }
-  console.log(maxSubstr)
-  console.log(maxSubstr.length)
- return maxSubstr.length
-};
 
-// lengthOfLongestSubstring('aaa')
+ return max_j - max_i + 1
+};// lengthOfLongestSubstring('aaa')
 // lengthOfLongestSubstring('')
 // lengthOfLongestSubstring('a331. sss')
 // lengthOfLongestSubstring('abcabcbb')
 lengthOfLongestSubstring('aud')
 lengthOfLongestSubstring('dvdf')
-lengthOfLongestSubstring('bbtablud')
-lengthOfLongestSubstring('wobgrovw')
+// lengthOfLongestSubstring('bbtablud')
+// lengthOfLongestSubstring('wobgrovw')
+// lengthOfLongestSubstring('abcabcbb')
+// lengthOfLongestSubstring('vqblqcb')
